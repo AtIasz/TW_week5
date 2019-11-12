17b17dc3943e3fc1f2bda8490383dd2120a3597e
Szanyi^2 <ifj.szanyi.sandor@gmail.com>
2019-11-12 13:59:00 +0100

    exit

--- ui.py
+++ ui.py
@@ -1,15 +1,17 @@
 def print_menu(title, options):
     print(title)
+    
     for i in range(len(options)):
         print('(' + str(i+1) + ')', end='')
         print(options[i])
-
+        
+    print("(0) Exit")
 
 def print_something_else(arg):
     pass
 
-def get_inputs(arg):
-    pass
+def get_inputs():
+    data_in=input()
 
 def print_error_message(message):
     print(message)
